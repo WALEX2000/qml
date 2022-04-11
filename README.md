9c540041c2acd7eaa9ab4e699749aa4d810ca3a1
Alex Carqueja <up201705049@fe.up.pt>
2022-04-11 16:03:00 +0100

    Added DVC and Git setup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+# QML
+## Setup
+To install for prod. run: pipx install . --force
+To install for dev. run:
+    pipenv shell
+    then, just call qml (It's already on the path)
+
 qml is a tool to quickstart machine learning projects, while supporting lightweight MLOps tasks
--- qml.py
+++ qml.py
@@ -1,8 +1,16 @@
+import string
 import typer
 import click
 import os
+import subprocess
+from subprocess import Popen, PIPE, STDOUT
+import sys
+import shutil
+import io
+from types import SimpleNamespace
+from collections import namedtuple
 
-def setupFolderStructure(name):
+def setupProjectStructure(name):
     mode = 0o777
     # Create Paths
     rootPath = os.path.join(os.getcwd(), name)
@@ -16,22 +24,57 @@ def setupFolderStructure(name):
     os.mkdir(srcPath, mode)
     os.mkdir(mlPrimitivesPath, mode)
     os.mkdir(pipelinesPath, mode)
+    # Add Template files
+    pipfile = os.path.dirname(os.path.abspath(__file__)) + "/Templates/Pipfile"
+    shutil.copy(pipfile, rootPath)
+    
+    Paths = namedtuple('Paths', ['rootPath', 'dataPath', 'srcPath', 'mlPrimitivesPath', 'pipelinesPath'])
+    return Paths(rootPath, dataPath, srcPath, mlPrimitivesPath, pipelinesPath)
+
+def CLIexec(cmd: string, execDir: string):
+    p = Popen(cmd, stdout = PIPE, stderr = STDOUT, shell = True, cwd=execDir)
+    reader = io.TextIOWrapper(p.stdout, encoding=None, newline='')
+    while not p.stdout.closed:
+        char = reader.read(1)
+        if(char == ''):
+            break
+        print(char, end='')
+
+def installDependencies(projRoot: string):
+    # Install dependencies with Pipenv
+    CLIexec('pipenv install', projRoot)
+
+def initDVC(projRoot: string):
+    CLIexec('dvc init', projRoot)
+
+def initGit(projRoot: string):
+    CLIexec('git init', projRoot)
+    CLIexec('git add *', projRoot)
+
+def gitCommit(message: string, projRoot: string):
+    cmd = 'git commit -m "' + message + '"'
+    CLIexec(cmd, projRoot)
 
 @click.command()
 @click.option('-n', '--name', type=str, help='Name of root project directory', default='Root_of_Project')
 def setup(name):
     print("Commencing Setup...")
-    setupFolderStructure(name)
-    # -- Add Template Files --
+    print('\n-> Creating Project Structure')
+    paths = setupProjectStructure(name)
     # -- Initialize git --
-    # -- Initialize DVC --
-
-
-    print("Setup Complete")
+    print('\n-> Installing Dependencies')
+    installDependencies(paths.rootPath)
+    print('\n-> Initiating Git')
+    initGit(paths.rootPath)
+    print('\n-> Initiating DVC')
+    initDVC(paths.rootPath)
+    print('\n-> Commiting setup to Git')
+    gitCommit('Setup Project!', paths.rootPath)
+    print("\n...Setup Complete!")
 
 @click.command()
 def stuff():
-    print("Doing a lot of stuff...")
+    print(setupProjectStructure("oioi"))
 
 
 app = typer.Typer()
