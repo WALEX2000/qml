fa14c542c818c8228f834442a07b237d964b8387
Alex Carqueja <up201705049@fe.up.pt>
2022-05-17 19:26:08 +0100

    start no longer appears when already inside an environment

--- qml.py
+++ qml.py
@@ -53,6 +53,8 @@ def addCommandsToQml(projRootPath : str, envConfDict : dict):
         if f is None: continue
         else: cli.command(name=commandName, context_settings=contextSettings)(f)
 
+showStart = True
+
 virtualEnvironment = os.environ.get('VIRTUAL_ENV')
 tail = None
 if virtualEnvironment is not None:
@@ -62,41 +64,43 @@ if tail == '.venv': # running inside what is possibly a qml environment
     envConfDict = getYAML(envConfLocation)
     if envConfDict is not None: # If we were able to read a env YAML file from the location of the .venv, then we can proceed
         addCommandsToQml(os.getcwd(), envConfDict)
+        showStart = False
 
-@cli.command()
-@click.option('-p', '--path', type=str, help='Name of root project directory relative to current directory', default=None)
-@click.option('-conf', '--config', type=str, help='Name of configuration file for setup of a new environment', default=None)
-def start(path, config):
-    if path is None: rootPath = os.getcwd()
-    else: rootPath = os.getcwd() + '/' + path
-    localConfigPath = rootPath + '/' + LOCAL_CONFIG_FILE_NAME
+if showStart:
+    @cli.command()
+    @click.option('-p', '--path', type=str, help='Name of root project directory relative to current directory', default=None)
+    @click.option('-conf', '--config', type=str, help='Name of configuration file for setup of a new environment', default=None)
+    def start(path, config):
+        if path is None: rootPath = os.getcwd()
+        else: rootPath = os.getcwd() + '/' + path
+        localConfigPath = rootPath + '/' + LOCAL_CONFIG_FILE_NAME
 
-    if config is None: # No config was specified (either create new, or boot up)
-        if(os.path.exists(localConfigPath)):
-            print('Environment Configuration Detected!')
-            configAssetPath = localConfigPath
-            setup = False # Pre-existing environment has already been setup
-        else:
-            configAssetPath = getEnvConfigPath('.default_env.yaml')
-            setup = True  # There's no Pre-existing environemnt
-    else: # A config was specified (create new)
-        setup = True
-        configFileName = '.' + config + '.yaml'
-        configAssetPath = getEnvConfigPath(configFileName)
-        if (not os.path.exists(configAssetPath)):
-            print('ERROR: Specified configuration file ' + config + ' could not be found in package assets')
-            return
+        if config is None: # No config was specified (either create new, or boot up)
+            if(os.path.exists(localConfigPath)):
+                print('Environment Configuration Detected!')
+                configAssetPath = localConfigPath
+                setup = False # Pre-existing environment has already been setup
+            else:
+                configAssetPath = getEnvConfigPath('.default_env.yaml')
+                setup = True  # There's no Pre-existing environemnt
+        else: # A config was specified (create new)
+            setup = True
+            configFileName = '.' + config + '.yaml'
+            configAssetPath = getEnvConfigPath(configFileName)
+            if (not os.path.exists(configAssetPath)):
+                print('ERROR: Specified configuration file ' + config + ' could not be found in package assets')
+                return
+            
+            if(os.path.exists(localConfigPath)): # An environment has alread been setup before
+                print("ERROR: provided a configuration file: '" + config + "' But there already exists one in this directory: '" + localConfigPath + "'")
+                return
         
-        if(os.path.exists(localConfigPath)): # An environment has alread been setup before
-            print("ERROR: provided a configuration file: '" + config + "' But there already exists one in this directory: '" + localConfigPath + "'")
-            return
-    
-    if(setup):  # Need to create new Env
-        print('No existing environemnt found. Will start a new one..')
-        createEnv(configAssetPath, rootPath)
-    
-    envDict = checkEnv(localConfigPath, rootPath)
-    loadEnv(envDict, rootPath)
+        if(setup):  # Need to create new Env
+            print('No existing environemnt found. Will start a new one..')
+            createEnv(configAssetPath, rootPath)
+        
+        envDict = checkEnv(localConfigPath, rootPath)
+        loadEnv(envDict, rootPath)
 
 @cli.command()
 def edit():
